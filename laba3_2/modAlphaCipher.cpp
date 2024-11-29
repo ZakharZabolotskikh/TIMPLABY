7d10c2108be4db899ef6f42ffe4c15c3fc4523bf
ZakharZabolotskikh <zabolotskikhpnz@gmail.com>
2024-11-29 19:33:52 +0300

    Update modAlphaCipher.cpp

--- laba3_2/modAlphaCipher.cpp
+++ laba3_2/modAlphaCipher.cpp
@@ -1,110 +1,110 @@
-#include <modAlphaCipher.h>
-Cipher::Cipher(const std::wstring & ws_key)
-{
-	std::wstring tmp(ws_key);
-	columns = getValidKey(tmp);
-}
-void Cipher::set_key(std::wstring & ws_key)
-{
-	columns = getValidKey(ws_key);
-}
-void Cipher::set_tableform(const std::wstring& open_text)
-{
-	len_text = open_text.size();
-	if (len_text>columns) {
-		rows = len_text/columns;
-		if (len_text%columns >0) rows+=1;
-	} else {
-		rows = 1;
-	}
-}
-std::wstring Cipher::encrypt(const std::wstring& open_text)
-{
-	std::wstring tmp(open_text);
-	tmp = getValidOpenText(tmp);
-	set_tableform(tmp);
-	std::wstring table_chiper[rows][columns];
-	int nums_text = 0;
-	for (int y=0; y<rows; y++) {
-		for (int x=0; x<columns; x++) {
-			if (nums_text < len_text) {
-				table_chiper[y][x] = tmp[nums_text];
-			} else {
-				table_chiper[y][x] = L"-";
-			}
-			nums_text++;
-		}
-	}
-	std::wstring encrypt_text;
-	for (int x=columns-1; x>=0; x--) {
-		for (int y=0; y<rows; y++) {
-			encrypt_text+=table_chiper[y][x];
-		}
-	}
-	return encrypt_text;
-}
-std::wstring Cipher::decrypt(const std::wstring& cipher_text)
-{
-	set_tableform(getValidCipherText(cipher_text));
-	std::wstring table_chiper[rows][columns];
-	int nums_text = 0;
-	for (int x=columns-1; x>=0; x--) {
-		for (int y=0; y<rows; y++) {
-			table_chiper[y][x] = cipher_text[nums_text];
-			nums_text++;
-		}
-	}
-	std::wstring decrypt_text;
-	for (int y=0; y<rows; y++) {
-		for (int x=0; x<columns; x++) {
-			if (table_chiper[y][x] != L"-")
-				decrypt_text+=table_chiper[y][x];
-		}
-	}
-	return decrypt_text;
-}
-inline int Cipher::getValidKey(std::wstring & ws_key)
-{
-	if (ws_key.empty())
-		throw cipher_error("Empty key");
-	std::string s_key = codec.to_bytes(ws_key);
-	for (auto & c:ws_key) {
-		if (!iswdigit(c)) {
-			throw cipher_error(std::string("Invalid key: ") + s_key);
-		}
-	}
-	int key = std::stoi(ws_key);
-	if (key<=0) {
-		throw cipher_error(std::string("Key must be greater than 0"));
-	}
-	return key;
-}
-inline std::wstring Cipher::getValidOpenText(const std::wstring & ws_open_text)
-{
-	std::wstring tmp;
-	for (auto c:ws_open_text) {
-		if (iswalpha(c)) {
-			if (iswlower(c))
-				tmp.push_back(towupper(c));
-			else
-				tmp.push_back(c);
-		}
-	}
-	if (tmp.empty())
-		throw cipher_error("Empty open text");
-	return tmp;
-}
-inline std::wstring Cipher::getValidCipherText(const std::wstring & ws_cipher_text)
-{
-	if (ws_cipher_text.empty())
-		throw cipher_error("Empty cipher text");
+#include "modAlphaCipher.h"
+#include <cmath>
+#include <cctype>
+#include <locale>
+#include <codecvt>
+#include <iostream>
+#include <string>
 
-	for (auto c:ws_cipher_text) {
-		if (!iswupper(c)) {
-			if (c!=L'-') {
-				throw cipher_error(std::string("Invalid cipher text"));
-			}
-		}
-	}
-	return ws_cipher_text;
+std::wstring RouteCipher::encrypt(const std::wstring& plaintext, int columns)
+{
+    columns = getValidColumns(columns);
+    std::wstring validatedPlainText = getValidPlainText(plaintext);
+    int rows = (validatedPlainText.length() + columns - 1) / columns;
+    std::wstring encryptedText;
+
+    for (int i = columns - 1; i >= 0; --i) {
+        for (int j = 0; j < rows; ++j) {
+            std::wstring::size_type index = j * columns + i;
+
+            if (index < validatedPlainText.length()) {
+                encryptedText += validatedPlainText[index];
+            }
+        }
+    }
+    return encryptedText;
+}
+
+std::wstring RouteCipher::decrypt(const std::wstring& ciphertext, int columns)
+{
+    columns = getValidColumns(columns);
+    std::wstring validatedCipherText = getValidCipherText(ciphertext);
+    int rows = static_cast<int>(std::ceil(static_cast<double>(validatedCipherText.length()) / columns));
+    std::wstring decryptedText;
+
+    for (int i = rows - 1; i >= 0; --i) {
+        for (int j = columns - 1; j >= 0; --j) {
+            std::wstring::size_type index = j * rows + (rows - i - 1);
+
+            if (index < validatedCipherText.length()) {
+                decryptedText += validatedCipherText[index];
+            }
+        }
+    }
+    return decryptedText;
+}
+
+int RouteCipher::getValidColumns(int columns)
+{
+    if (columns <= 0)
+        throw CipherError(L"Invalid number of columns");
+    return columns;
+}
+
+std::wstring RouteCipher::getValidPlainText(const std::wstring & s)
+{
+    for (auto c : s) {
+        if (std::iswdigit(c)) {
+            throw CipherError(L"Digits are not allowed in plaintext");
+        }
+    }
+
+    std::wstring tmp;
+    for (auto c : s) {
+        if (std::iswalpha(c)) {
+            tmp.push_back(std::towupper(c));
+        } else {
+            throw CipherError(L"Invalid character in plaintext");
+        }
+    }
+
+    if (tmp.empty())
+        throw CipherError(L"Empty plaintext");
+    
+    return tmp;
+}
+
+std::wstring RouteCipher::getValidCipherText(const std::wstring & s)
+{
+    for (auto c : s) {
+        if (std::iswdigit(c)) {
+            throw CipherError(L"Digits are not allowed in ciphertext");
+        }
+    }
+
+    std::wstring tmp;
+    for (auto c : s) {
+        if (std::iswalpha(c) && std::iswupper(c)) {
+            tmp.push_back(c);
+        } else {
+            throw CipherError(L"Invalid character in ciphertext");
+        }
+    }
+
+    if (tmp.empty())
+        throw CipherError(L"Empty ciphertext");
+    
+    return tmp;
+}
+
+std::wstring RouteCipher::string_to_wstring(const std::string& str)
+{
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+    return converter.from_bytes(str);
+}
+
+std::string RouteCipher::wstring_to_string(const std::wstring& wstr)
+{
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+    return converter.to_bytes(wstr);
 }
